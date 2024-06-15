ea6731f6081a87d68baa2270ba76731139a392a2
Mark <ajineer@outlook.com>
2024-06-15 13:25:30 -0600

    switched over databse from mongo back to flask, I want to work with python flask again and have relational models to work with.

---
+++ server/models/User.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from config import db, bcrypt
+
+
+class User(db.Model, SerializerMixin):
+    __table__ = "users"
+    serialize_rules = ("-_password_hash",)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception("Password hashes may not be viewed")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
+    @validate("email")
+    def validate_email(self, key, email):
+        if not email or not isinstance(email, str):
+            raise ValueError("Email must be a non-empty string.")
+        return email
